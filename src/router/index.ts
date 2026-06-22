9dcbb32f742a8d8e142369ef23630479d27619d4
Floris Bartra <floris.bartra@gmail.com>
2026-06-22 11:09:59 +0200

    Update title when changing route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import HomeView from '../views/HomeView.vue';
+import HomeView from '@/views/HomeView.vue';
+
+const INITIAL_TITLE = document.title;
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,8 +11,16 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+      meta: { title: 'Home' },
     },
   ],
+});
+
+router.beforeEach((to, _, next) => {
+  const { title } = to.meta;
+  const resolvedTitle = typeof title === 'function' ? title(to) : title;
+  document.title = `${resolvedTitle} \u2022 ${INITIAL_TITLE}`;
+  next();
 });
 
 export default router;
